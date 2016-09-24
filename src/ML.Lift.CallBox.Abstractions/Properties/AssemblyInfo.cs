3276d85f843787f7f6c10fab725adb62455cb115
Michael Love <mlove@leisurelink.com>
2016-09-24 00:12:44 -0600

    Initial commit with the solution and stubbed in projects.  Missing all tests projects still.

---
+++ src/ML.Lift.CallBox.Abstractions/Properties/AssemblyInfo.cs
@@ -0,0 +1,19 @@
+﻿using System.Reflection;
+using System.Runtime.CompilerServices;
+using System.Runtime.InteropServices;
+
+// General Information about an assembly is controlled through the following
+// set of attributes. Change these attribute values to modify the information
+// associated with an assembly.
+[assembly: AssemblyConfiguration("")]
+[assembly: AssemblyCompany("")]
+[assembly: AssemblyProduct("ML.Lift.CallBox.Abstractions")]
+[assembly: AssemblyTrademark("")]
+
+// Setting ComVisible to false makes the types in this assembly not visible
+// to COM components.  If you need to access a type in this assembly from
+// COM, set the ComVisible attribute to true on that type.
+[assembly: ComVisible(false)]
+
+// The following GUID is for the ID of the typelib if this project is exposed to COM
+[assembly: Guid("a08e640a-d28e-44b5-a82d-334c039e6c77")]
