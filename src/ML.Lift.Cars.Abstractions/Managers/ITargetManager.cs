18da402826f1675b4822efa22ce018ba9081b8c9
Michael Love <mlove@leisurelink.com>
2016-09-28 10:28:38 -0600

    Adding the Dispatch vertical back.

---
+++ src/ML.Lift.Cars.Abstractions/Managers/ITargetManager.cs
@@ -0,0 +1,11 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace ML.Lift.Cars.Abstractions.Managers
+{
+    interface ITargetManager
+    {
+    }
+}
